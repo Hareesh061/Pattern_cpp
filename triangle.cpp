9348dc89b970a0b7bfd68695ebfddd51f7819517
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-08-25 10:31:57 +0530

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -12,6 +12,18 @@ for(int i=0;i<n;i++){
 	for(int k=0;k<2*i+1;k++){
 		cout<<"* ";}
 cout<<endl;
+
+// for(int i=0;i<n;i++){
+// 	for(int j=0;j<2 * i; j++){
+//		cout<<" ";}
+//		for(int k=0;k<2*(n-i)-1;k++){
+//          		cout<<"* ";}
+//  	cout<<endl;}
+
+
+
+
+	
    }
 
 }
