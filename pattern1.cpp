6a0ef0530996e7453192e5551cba2022ce4deaef
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-10-12 19:56:09 +0530

    Update pattern1.cpp

--- pattern1.cpp
+++ pattern1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 int n;
-cout<<"enter the number"<<endl;
+cout<<"enter the number of your choice"<<endl;
 cin>>n;
 for(int row=0;row<=n;row++){
   for(int col=0;col<row;col++){
